b907710d1d37be6cafefb878dd78e06cf1ab79fa
CrazyWolf23 <acooper340@gmail.com>
2016-02-14 15:18:27 -0500

    Updated Redstone Energy API

--- API's/Redstone Energy/Redstone Energy.js
+++ API's/Redstone Energy/Redstone Energy.js
@@ -6,15 +6,8 @@ To view a copy of this license,
 visit http://creativecommons.org/licenses/by-sa/4.0/.
 */
 
-var version = "1.0";
-var energyBlocksId = [];
-var energyBlocksD = [];
+var version = "1.0.1";
 var energyBlocksInWorld = {};
-
-function setEnergyBlock(id,d){
-	energyBlocksId.push(id);
-	energyBlocksD.push(d);
-}
 
 function addEnergyBlockToWorld(x,y,z,id,d){
 	energyBlocksInWorld[x,y,z] = {
@@ -37,4 +30,20 @@ function removeEnergyFromBlock(x,y,z,amt){
 	if(energyBlocksInWorld[x,y,z]){
 		energyBlocksInWorld[x,y,z].energy-=amt;
 	}
+}
+
+function getEnergy(x,y,z){
+	return energyBlocksInWorld[x,y,z].energy;
+}
+
+function getEnergyBlockId(x,y,z){
+	return energyBlocksInWorld[x,y,z].id;
+}
+
+function getEnergyBlockData(x,y,z){
+	return energyBlocksInWorld[x,y,z].d;
+}
+
+function getVersion(){
+	return version;
 }
